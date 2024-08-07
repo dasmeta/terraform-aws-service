575938243d17e50472d9e61bdcd4be22e0b518ef
Aram Karapetyan <aram@dasmeta.com>
2024-08-07 14:10:55 +0400

    fix(DMVP-4196): Superset service setup

--- modules/superset/locals.tf
+++ modules/superset/locals.tf
@@ -1,88 +1,80 @@
 locals {
   redis_config = {
-    "superset" : {
-      "postgresql" : {
-        "enabled" : var.postgress_config.create,
-      },
-      "redis" : {
-        "enabled" : var.redis_config.create,
-      },
-      "supersetNode" : {
-        "connections" : {
-          "db_host" : "${var.name}-postgresql"
-          "db_port" : "5432"
-          "db_user" : "superset"
-          "db_pass" : "superset"
-          "db_name" : "superset"
-          "redis_host" : var.redis_config.host
-          "redis_port" : var.redis_config.port
-        }
+    "postgresql" : {
+      "enabled" : var.postgress_config.create,
+    },
+    "redis" : {
+      "enabled" : var.redis_config.create,
+    },
+    "supersetNode" : {
+      "connections" : {
+        "db_host" : "${var.name}-postgresql"
+        "db_port" : "5432"
+        "db_user" : "superset"
+        "db_pass" : "superset"
+        "db_name" : "superset"
+        "redis_host" : var.redis_config.host
+        "redis_port" : var.redis_config.port
       }
     }
   }
 
   postgress_config = {
-    "superset" : {
-      "postgresql" : {
-        "enabled" : var.postgress_config.create,
-      },
-      "redis" : {
-        "enabled" : var.redis_config.create,
-      },
-      "supersetNode" : {
-        "connections" : {
-          "db_host" : var.postgress_config.host
-          "db_port" : var.postgress_config.port
-          "db_user" : var.postgress_config.user
-          "db_pass" : var.postgress_config.pass
-          "db_name" : var.postgress_config.name
-          "redis_host" : "${var.name}-redis-headless"
-          "redis_port" : "6379"
-        }
+    "postgresql" : {
+      "enabled" : var.postgress_config.create,
+    },
+    "redis" : {
+      "enabled" : var.redis_config.create,
+    },
+    "supersetNode" : {
+      "connections" : {
+        "db_host" : var.postgress_config.host
+        "db_port" : var.postgress_config.port
+        "db_user" : var.postgress_config.user
+        "db_pass" : var.postgress_config.pass
+        "db_name" : var.postgress_config.name
+        "redis_host" : "${var.name}-redis-headless"
+        "redis_port" : "6379"
       }
     }
   }
 
   redis_postgress_config = {
-    "superset" : {
-      "postgresql" : {
-        "enabled" : var.postgress_config.create,
-      },
-      "redis" : {
-        "enabled" : var.redis_config.create,
-      },
-      "supersetNode" : {
-        "connections" : {
-          "db_host" : var.postgress_config.host
-          "db_port" : var.postgress_config.port
-          "db_user" : var.postgress_config.user
-          "db_pass" : var.postgress_config.pass
-          "db_name" : var.postgress_config.name
-          "redis_host" : var.redis_config.host
-          "redis_port" : var.redis_config.port
-        }
+    "postgresql" : {
+      "enabled" : var.postgress_config.create,
+    },
+    "redis" : {
+      "enabled" : var.redis_config.create,
+    },
+    "supersetNode" : {
+      "connections" : {
+        "db_host" : var.postgress_config.host
+        "db_port" : var.postgress_config.port
+        "db_user" : var.postgress_config.user
+        "db_pass" : var.postgress_config.pass
+        "db_name" : var.postgress_config.name
+        "redis_host" : var.redis_config.host
+        "redis_port" : var.redis_config.port
       }
     }
   }
 
   default_config = {
-    "superset" : {
-      "postgresql" : {
-        "enabled" : "true",
-      },
-      "redis" : {
-        "enabled" : "true",
-      },
-      "supersetNode" : {
-        "connections" : {
-          "db_host" : "${var.name}-postgresql"
-          "db_port" : "5432"
-          "db_user" : "superset"
-          "db_pass" : "superset"
-          "db_name" : "superset"
-          "redis_host" : "${var.name}-redis-headless"
-          "redis_port" : "6379"
-        }
+    "postgresql" : {
+      "enabled" : "true",
+    },
+    "redis" : {
+      "enabled" : "true",
+    },
+    "supersetNode" : {
+      "connections" : {
+        "db_host" : "${var.name}-postgresql"
+        "db_port" : "5432"
+        "db_user" : "superset"
+        "db_pass" : "superset"
+        "db_name" : "superset"
+        "redis_host" : "${var.name}-redis-headless"
+        "redis_port" : "6379"
       }
     }
   }
